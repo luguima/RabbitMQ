61aa8d0d36c771ed4c57537ceca19cefda181f3f
luguima <l.g.mattos@hotmail.com>
2021-08-31 17:37:50 -0300

    First and Second tutorial

---
+++ NewTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace NewTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Sending");
+
+            var factory = new ConnectionFactory() { HostName = "ubuntuserver", UserName = "guest", Password = "M@TTOSs01" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    var message = GetMessage(new string[] { "Message", "".PadLeft(i + 1, '.'), (i + 1).ToString() });
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "task_queue",
+                                         basicProperties: properties,
+                                         body: body);
+                }
+
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ RabbitMQ.Send/Send.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Send
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Sending");
+
+            var factory = new ConnectionFactory() { HostName = "ubuntuserver", UserName = "guest", Password = "M@TTOSs01"  };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
